c40c32c5829d5939495801ff558247732dabf5b6
mrkhanboyy <mrkhanboyy@gmail.com>
2020-07-13 21:51:58 -0700

    july 14 ,updated bean classes

---
+++ TodoWebApp/src/com/todo/bean/Todo.java
@@ -0,0 +1,35 @@
+package com.todo.bean;
+
+public class Todo {
+	
+	private int todoId;
+	private String subject,description;
+	private String date;
+	
+	
+	public int getTodoId() {
+		return todoId;
+	}
+	public void setTodoId(int todoId) {
+		this.todoId = todoId;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+}
--- TodoWebApp/src/com/todo/bean/User.java
+++ TodoWebApp/src/com/todo/bean/User.java
@@ -1,5 +1,34 @@
 package com.todo.bean;
 
 public class User {
+	
+	private int userId;
+	private String name,email,password;
+	
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
